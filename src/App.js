0965dcd17f566317dbd4c23aad7089fc6d43aec6
LutfiNurAnggreini <97815780+LutfiNurAnggreini@users.noreply.github.com>
2022-01-16 14:39:39 +0700

    Workshop

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import "./App.css";
+import "./Assets/App.scss";
+
+function App() {
+  return (
+  <div className="container">
+    <div className="header">header</div>
+
+    <div className="Section">
+      <div className="Card Section-1">1</div>
+      <div className="Card Section-2">2</div>
+      <div className="Card Section-3">3</div>
+      <div className="Card Section-4">4</div>
+      <div className="Card Section-5">5</div>
+      <div className="Card Section-6">6</div>
+      <div className="Card Section-7">7</div>
+      <div className="Card Section-8">8</div>
+      <div className="Card Section-9">9</div>
+    </div>;
+    
+    <div className="Body">
+      <div className="Body-Anak Body-1">1</div>
+      <div className="Body-Anak Body-2">2</div>
+      <div className="Body-Anak Body-3">3</div>
+    </div>
+
+</div>
+
+
+  )
+}
+export default App;
